24df41df80713e0fae5b2d99913bd3ea681f68a5
gabriegonza <gonzalezgithub@gmail.com>
2023-07-18 16:35:39 -0300

    Adicionando um System.out.println

--- Desafio 3/src/Main.java
+++ Desafio 3/src/Main.java
@@ -9,6 +9,7 @@ public class Main {
         System.out.println("Digite o valor alvo: ");
         int k = scanner.nextInt();
 
+        System.out.println("Digite os valores do array");
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = scanner.nextInt();
